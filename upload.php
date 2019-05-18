550100997707a33d44b5038610e4204ee51c8cd5
kevinriyanto <43446573+kevinriyanto@users.noreply.github.com>
2019-05-18 12:27:07 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -31,7 +31,6 @@
 <head>
     <title>Analyze Sample</title>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"> </script>
 </head>
 <body>
  
